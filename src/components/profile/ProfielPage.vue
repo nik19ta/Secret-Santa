c29142d2da816796375fce8f6eaa80e808492146
Nik19ta <nikhvatov19@gmail.com>
2020-05-08 17:00:50 +0300

    поправил профиль

--- src/components/profile/ProfielPage.vue
+++ src/components/profile/ProfielPage.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="body">
-        <div class="cards">
-            <ProfileCard />
-            <ProfilePresentCard />
-        </div>
-    </div>
+<div class="body">
+  <div class="cards">
+    <ProfileCard />
+    <ProfilePresentCard />
+  </div>
+</div>
 </template>
 
 <script>
 import ProfileCard from './ProfileCard'
 import ProfilePresentCard from './ProfilePresentCard'
-    export default {
-        name: 'ProfilePage',
-        components: {
-            ProfileCard, ProfilePresentCard
-        }
-    }
+export default {
+  name: 'ProfilePage',
+  components: {
+    ProfileCard,
+    ProfilePresentCard
+  }
+}
 </script>
 
 <style scoped>
 .body {
-    height: 1320px;
-    background-color: #00A460;
+  min-height: 100vh;
+  background-color: #00A460;
+  padding-bottom: 30px;
 }
+
 .cards {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
